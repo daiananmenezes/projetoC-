3f8943775b63d78646062d4d4fab27da6f536419
srpulga <edu.tads@outlook.com>
2021-09-28 20:59:53 -0300

    Create initial usuario components

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditarUsuarioComponent } from './components/usuario/editar-usuario/editar-usuario.component';
+import { CadastrarUsuarioComponent } from './components/usuario/cadastrar-usuario/cadastrar-usuario.component';
+import { RemoverUsuarioComponent } from './components/usuario/remover-usuario/remover-usuario.component';
+import { ListarUsuarioComponent } from './components/usuario/listar-usuario/listar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarUsuarioComponent,
+    CadastrarUsuarioComponent,
+    RemoverUsuarioComponent,
+    ListarUsuarioComponent
   ],
   imports: [
     BrowserModule,
