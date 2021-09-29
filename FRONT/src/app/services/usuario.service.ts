59760aefcbef0822539ac40818ba7728dd3e7def
srpulga <edu.tads@outlook.com>
2021-09-28 21:23:28 -0300

    Create model and service for usuario

---
+++ FRONT/src/app/services/usuario.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Usuario } from "../models/usuario";
+
+@Injectable({
+    providedIn: "root",
+})
+export class UsuarioService {
+    private baseUrl = "http://localhost:5000/api/usuario";
+
+    constructor(private http: HttpClient) {}
+
+    list(): Observable<Usuario[]> {
+        return this.http.get<Usuario[]>(`${this.baseUrl}/list`);
+    }
+
+    create(usuario: Usuario): Observable<Usuario> {
+        return this.http.post<Usuario>(`${this.baseUrl}/create`, usuario);
+    }
+
+    find(id: Number): Observable<Usuario> {
+        return this.http.get<Usuario>(`${this.baseUrl}/getbyid/${id}`);
+    }
+
+    update(usuario: Usuario): Observable<Usuario> {
+        return this.http.put<Usuario>(`${this.baseUrl}/update`, usuario);
+    }
+    
+    delete(usuario: Usuario): Observable<Usuario> {
+        return this.http.delete<Usuario>(`${this.baseUrl}/update/${usuario.nome}`);
+    }
+
+}
